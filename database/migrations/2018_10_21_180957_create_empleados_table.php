62e64d091159a17f59bfa03120ba96ea8cb3dbba
JuanCamilo <operations761@gmail.com>
2019-03-07 10:15:50 -0500

    se aniaden migrations

---
+++ database/migrations/2018_10_21_180957_create_empleados_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('identificacion');
+            $table->string('name',50);
+            $table->string('apellido',50);
+            $table->integer('telefono');
+            $table->string('correoelectronico',50);
+            $table->integer('id_tipovinculacion')->unsigned();
+            $table->foreign('id_tipovinculacion')->references('id')->on('tipo_vinculacions');
+            $table->date('fechadenacimiento');
+            $table->double('salario');
+            $table->integer('id_cargo')->unsigned();
+            $table->foreign('id_cargo')->references('id')->on('cargos'); 
+            $table->integer('id_sede')->unsigned();
+            $table->foreign('id_sede')->references('id')->on('sedes');
+            $table->date('fechadeingreso');
+            $table->enum('estado',['ACTIVO','INACTIVO']);
+            $table->enum('genero',['MASCULINO','FEMENINO']); 
+            $table->timestamps();
+            $table->primary('id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
---
+++ database/migrations/2018_10_21_181224_create_ausentismos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAusentismosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */ 
+    public function up()
+    {
+        Schema::create('ausentismos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('fecha_registro');
+            $table->integer('id_empleado');
+            $table->foreign('id_empleado')->references('id')->on('empleados');
+            $table->integer('id_tipoausentismo')->unsigned();
+            $table->foreign('id_tipoausentismo')->references('id')->on('tipoausentismos');                        
+            $table->date('fecha_inicio');
+            $table->integer('tiempo_ausencia');
+            $table->double('costo_ausencia');
+            $table->enum('Grado',['LEVE','SEVERO']);
+            $table->text('observacion',100);
+            $table->unique('id');
+
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ausentismos');
+    }
+}
