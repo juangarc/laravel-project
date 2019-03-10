08519d62f1355a76de87e452a32e3539ee7bd63f
Your Name <you@example.com>
2019-03-10 10:39:24 -0500

    asd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+<<<<<<< HEAD
 
+=======
+>>>>>>> asd
 class AppServiceProvider extends ServiceProvider
 {
     /**
