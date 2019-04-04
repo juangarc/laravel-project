8e30e4dab56355b4f28a8d3902797f17be65085a
zerg1496 <mosquera.ivan@correounivalle.edu.co>
2019-04-04 15:49:20 -0500

    RolesSeeder customer

--- database/seeds/RolesSeeder.php
+++ database/seeds/RolesSeeder.php
@@ -28,10 +28,29 @@ class RolesSeeder extends Seeder
             'updated_at' => now()
         ]);
 
+        DB::table('roles')->insert([
+            'name' => 'customer',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
         DB::table('model_has_roles')->insert([
             'role_id' => 1,
             'model_type' => 'App\User',
             'model_id' => 1
+        ]);
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => 2,
+            'model_type' => 'App\User',
+            'model_id' => 2
+        ]);
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => 3,
+            'model_type' => 'App\User',
+            'model_id' => 3
         ]);
 
 
